be2b7b784a0587aa49858f059a470b59feb181d0
Daniel Dolan <danieldolan7@gmail.com>
2020-05-28 21:14:12 -0400

    Fixing #07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,7 +1,7 @@
 function countOfAllIndexMatchingNumbers(nums) {
   let count = 0;
   for (var i = 0; i < nums.length; i++) {
-    if (nums.indexOf([i]) == nums[i]){
+    if (i == nums[i]){
       count++;
     }
   }
